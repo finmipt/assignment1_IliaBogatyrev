492b970c51cab7117056a2e6ccab317a616a1dd6
Ilia Bogatyrev <finmipt@gmail.com>
2022-12-27 20:08:23 +0200

    Добавьте файлы проекта.

---
+++ assignment1_IliaBogatyrev/Form1.cs
@@ -0,0 +1,35 @@
+namespace assignment1_IliaBogatyrev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Stockholm";
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Helsinki";
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            label2.Text = "Tallinn";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
